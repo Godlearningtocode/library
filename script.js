4704c5c63fa07a22c8a0cda8da9d31ed98cdc00b
Godlearningtocode <shivam.ayush.agarwal@gmail.com>
2023-12-08 02:20:55 +0530

    updated class for constructor

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const myLibrary = [];
 
-function Book(title, author, page, read) {
-    this.title = title,
-    this.author = author,
-    this.page = page,
-    this.read = read
+class Book {
+    constructor(title, author, page, read) {
+        this.title = title,
+        this.author = author,
+        this.page = page,
+        this.read = read
+    }
 }
 
 let addBtn = document.getElementById('addBtn');
