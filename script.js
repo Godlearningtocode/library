b0922e84d57b8098ef1c7c4be7d4a3f53ef43d58
Godlearningtocode <shivam.ayush.agarwal@gmail.com>
2024-01-08 14:37:29 +0530

    fixed logic and added form validation

--- script.js
+++ script.js
@@ -28,24 +28,44 @@ addButton.addEventListener('click', () => {
 //2. Click event for console logging myLibrary array when add button on form is clicked
 document.body.addEventListener('click', (event) => {
     if(event.target.id === 'submitButton') {
-        if(document.querySelector('#bookContainer') === null) {
-            const bookContainer = document.createElement('div');
-            bookContainer.id = 'bookContainer';
-            mainCard.prepend(bookContainer);
+        const bookForm = document.querySelector('.form');
+        if(bookForm.checkValidity() === true) {
+            if(document.querySelector('#bookContainer') === null) {
+                const bookContainer = document.createElement('div');
+                bookContainer.id = 'bookContainer';
+                mainCard.prepend(bookContainer);
+            }
+    
+            let titleValue = document.querySelector('#titleInput').value;
+            let authorInput = document.querySelector('#authorInput').value;
+            let pageInput = document.querySelector('#pageInput').value;
+            let readInput = document.querySelector('#readInput').value;
+        
+            let book = new Book(titleValue, authorInput, pageInput, readInput);
+            myLibrary.push(book);
+    
+            bookDisplay(book);
+            removeBookForm();
+        } else {
+            bookForm.reportValidity();
         }
 
-        let titleValue = document.querySelector('#titleInput').value;
-        let authorInput = document.querySelector('#authorInput').value;
-        let pageInput = document.querySelector('#pageInput').value;
-        let readInput = document.querySelector('#readInput').value;
-    
-        let book = new Book(titleValue, authorInput, pageInput, readInput);
-        myLibrary.push(book);
-
-        bookDisplay(book);
-        removeBookForm();
-
         event.preventDefault();
+    }
+})
+
+//3. Click event for removing book from display and from the array
+document.body.addEventListener('click', (event) => {
+    if(event.target.classList.contains('removeBook') === true) {
+        removeBook(event, myLibrary);
+    }
+})
+
+//4. Click event for changing the read status of the book
+document.body.addEventListener('click', (event) => {
+    if(event.target.classList.contains('bookRead') === true) {
+        readStatus(event, myLibrary);
+        console.log(myLibrary)
     }
 })
 
@@ -58,24 +78,34 @@ function addBookForm() {
     const titleLabel = document.createElement('label');
     titleLabel.classList.add('titleLabel');
     titleLabel.innerText = 'What is the Title?';
+    titleLabel.htmlFor = 'titleInput';
     const titleInput = document.createElement('input');
     titleInput.type = 'text';
     titleInput.id = 'titleInput';
+    titleInput.minLength = 1;
+    titleInput.required = true;
     const authorLabel = document.createElement('label');
     authorLabel.classList.add('authorLabel');
     authorLabel.innerText = 'Who is the Author?';
+    authorLabel.htmlFor = 'authorInput'
     const authorInput = document.createElement('input');
     authorInput.id = 'authorInput';
     authorInput.type = 'text';
+    authorInput.minLength = 1;
+    authorInput.required = true;
     const pageLabel = document.createElement('label');
     pageLabel.classList.add('pageLabel');
     pageLabel.innerText = 'How many pages is the book?';
+    pageLabel.htmlFor = 'pageInput';
     const pageInput = document.createElement('input');
     pageInput.id = 'pageInput';
-    pageInput.type = 'text';
+    pageInput.type = 'number';
+    pageInput.minLength = 1;
+    pageInput.required = true;
     const readLabel = document.createElement('label');
     readLabel.classList.add('readLabel');
     readLabel.innerText = 'Have you completed reading?';
+    readLabel.htmlFor = 'readInput';
     const readInput = document.createElement('SELECT');
     readInput.id = 'readInput';
     const yesOption = document.createElement('option');
@@ -84,6 +114,7 @@ function addBookForm() {
     const noOption = document.createElement('option');
     noOption.value = 'no';
     noOption.innerText = 'No.';
+    noOption.selected = true;
     const submitButton = document.createElement('button');
     submitButton.id = 'submitButton';
     submitButton.innerText = 'Submit';
@@ -141,6 +172,37 @@ function removeBookForm() {
 }
 
 //4. Function to remove book from library and display
-function removeBook() {
-    const removedBook = document.
+function removeBook(event, myLibrary) {
+    const removedBook = event.target;
+    removedBook.parentElement.parentElement.removeChild(removedBook.parentElement);
+
+    const bookTitle = removedBook.parentElement.firstChild;
+
+    for(let i = 0; i < myLibrary.length; i++) {
+        if(myLibrary[i].title === bookTitle.innerText) {
+            myLibrary.splice(i, 1);
+            console.log(1)
+        }
+    }
+}
+
+//4. Function for changing the read Status of the book
+function readStatus(event, myLibrary) {
+    const readButton = event.target;
+    const bookTitle = readButton.parentElement.firstChild;
+    if(readButton.innerText === 'yes') {
+        for(let i = 0; i < myLibrary.length; i++) {
+            if(myLibrary[i].title === bookTitle.innerText) {
+                myLibrary[i].read = 'no';
+            }
+        }
+        readButton.innerText = 'no';
+    } else if(readButton.innerText === 'no') {
+        for(let i = 0; i < myLibrary.length; i++) {
+            if(myLibrary[i].title === bookTitle.innerText) {
+                myLibrary[i].read = 'yes';
+            }
+        }
+        readButton.innerText = 'yes';
+    }
 }
